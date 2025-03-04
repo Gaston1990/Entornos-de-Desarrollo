b8db9d3c07f86a39244bcd2fa7296a449ae7ec8f
Gaston1990 <gastongarciacorleto@gmail.com>
2025-03-04 09:30:04 +0100

    Borrado seguro del metodo borrarDatos

--- src/Inventario/Inventario.java
+++ src/Inventario/Inventario.java
@@ -2,24 +2,48 @@ package Inventario;
 
 import Inventario.Producto;
 import java.util.ArrayList;
-
+/**
+ * Clase para representar inventario de productos
+ * permite agregar productos y mostrarlos
+ * 
+ * @author Gaston
+ * @version 1.0
+ * @since 04-03-2025
+ */
 public class Inventario {
+    /** Lista de productos en el inventario */
     public ArrayList<Producto> listaProductos;
 
+    /**
+     * Constructor inventario
+     * inicializa lista vacia de productos
+     */
     public Inventario() {
         listaProductos = new ArrayList<>();
     }
 
+    /**
+     * Añade un producto al inventario
+     * @param p Producto que se va a agregar
+     */
     public void agregar(Producto p) {
         listaProductos.add(p);
     }
 
+    /**
+     * Mostrar informacion de los productos del inventario
+     */
     public void mostrar() {
         for (int i = 0; i < listaProductos.size(); i++) {
             listaProductos.get(i).mostrarInformacion();
         }
     }
 
+    /**
+     * Metodo principal, prueba de funcionalidads
+     * 
+     * @param args 
+     */
     public static void main(String[] args) {
         Inventario inventario = new Inventario();
         Producto p1 = Producto.crearProducto("Laptop", 1200.99, 10);
--- src/Inventario/Producto.java
+++ src/Inventario/Producto.java
@@ -1,34 +1,62 @@
 package Inventario;
 
+/**
+ * Clase para representar productos con nombre, precio y stock
+ * 
+ * @author Gaston
+ * @version 1.0
+ * @since 04-03-2025
+ */
 public class Producto {
-    
+    /** Nombre del producto*/
     private String nombre;
+    /** Precio del producto*/
     private double precio;
+    /** Stock disponible del producto*/
     private int stock;
 
-    // Metodo de fabrica implementado
+    /**
+     * Metodo de fabrica para crear nuevo producto
+     * 
+     * @param nombre Nombre del producto
+     * @param precio Precio del producto
+     * @param stock Stock del producto
+     * @return nueva instancia del producto
+     */
     public static Producto crearProducto(String nombre, double precio, int stock) {
         return new Producto(nombre, precio, stock);
     }
 
+    /**
+     * Constructor privado de la clase producto
+     * 
+     * @param nombre Nombre del producto
+     * @param precio Precio del producto
+     * @param stock Stock del producto
+     */
     private Producto(String nombre, double precio, int stock) {
         this.nombre = nombre;
         this.precio = precio;
         this.stock = stock;
     }
 
+    /**
+     * Muestra informacion del producto en consola
+     */
     public void mostrarInformacion() {
         System.out.println("Producto: " + getNombre() + " | Precio: " + getPrecio() + " | Stock: " + getStock());
     }
     
     /**
-     * @return the nombre
+     * Obtiene nombre del producto
+     * @return nombre del producto
      */
     public String getNombre() {
         return nombre;
     }
 
     /**
+     * Establece nombre del producto
      * @param nombre the nombre to set
      */
     public void setNombre(String nombre) {
@@ -36,28 +64,32 @@ public class Producto {
     }
 
     /**
-     * @return the precio
+     * Obtiene precio del producto
+     * @return precio
      */
     public double getPrecio() {
         return precio;
     }
 
     /**
-     * @param precio the precio to set
+     * Establece precio del producto 
+     * @param precio nuevo precio de producto
      */
     public void setPrecio(double precio) {
         this.precio = precio;
     }
 
     /**
-     * @return the stock
+     * Obtiene cantidad de stock del producto
+     * @return  cantidad de stock
      */
     public int getStock() {
         return stock;
     }
 
     /**
-     * @param stock the stock to set
+     * Establece cantidad de stock del producto
+     * @param stock nueva cantidad de stock
      */
     public void setStock(int stock) {
         this.stock = stock;
